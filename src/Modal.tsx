33bad91a56ea01520f063f3e7f96e25858063f52
Abubakar <abubakkeramjad10@gmail.com>
2024-09-20 13:47:59 +0500

    fixed deployment errors

--- src/Modal.tsx
+++ src/Modal.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState } from "react";
 import { FaChevronDown, FaChevronUp, FaTimes } from "react-icons/fa";
 
 interface MediaItem {
